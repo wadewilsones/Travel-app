a074178b3d781b31b2991ffc039ec7fa76097c6e
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-04-24 09:46:46 -0700

    Update server.js
    Set up the server.

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser'); 
+const cors = require('cors');
 
+
+app.use (bodyParser.urlencoded ({extended:false}));
+app.use (bodyParser.json());
+app.use (cors());
+app.use (express.static('../../src/client'));
+
+
+//Set up a server
+
+const port = 3000;
+const server = app.listen (port, listening);
+function listening () {
+    console.log ('Server is running on localhost:' + port)
+}
+
+app.get (
+    ('/', function (req,res){
+        res.send ("hello world")
+    }) 
+)
