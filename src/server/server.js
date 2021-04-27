9409c4a410ecbc8bd2b1fa21e484f9611f6e483d
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-04-27 11:04:21 -0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -12,14 +12,35 @@ app.use (express.static('../../src/client'));
 
 //Set up a server
 
-const port = 3000;
+const port = 3100;
 const server = app.listen (port, listening);
 function listening () {
     console.log ('Server is running on localhost:' + port)
 }
 
-app.get (
-    ('/', function (req,res){
-        res.send ("hello world")
-    }) 
+
+//Set up API links
+
+const baseGeoUrl = 'api.geonames.org/searchJSON?';
+const username = 'wadewilso'; // hide later
+const geoPar = "&maxRows=1";
+
+//Set up get/post routes
+
+let projectData = {};
+
+app.get ("/coordinates", function getData(req,res) {
+    res.send ('')}
 )
+
+app.post ('/info', addData);
+ async function addData (req,res) {
+    const data = req.body;
+    let projectData = data;
+    const geoApi = await fetch (baseGeoUrl+place.value+username+geoPar, {method:'POST'})
+    try {
+        const geoData = await geoApi.json();
+        projectData['long'] = geoData.geonames[0].lng;
+    }
+    catch (error) {console.log('Error', error)}
+ }
