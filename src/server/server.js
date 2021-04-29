de9d1c8ce92dbfb181ac76ea1561d49a41b1fabc
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-04-28 20:50:08 -0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -27,7 +27,7 @@ const baseGeoUrl = 'http://api.geonames.org/searchJSON?';
 const username = 'wadewilso'; // hide later
 const geoP = "&maxRows=1";
 
-const baseWeatherUrl = 'https://api.weatherbit.io/v2.0/forecast/daily';
+const baseWeatherUrl = 'https://api.weatherbit.io/v2.0/current?';
 const apiK = '5a6a216b84a546b88c3ee16e59c33dd0';// hide later
 
 
@@ -35,53 +35,75 @@ const apiK = '5a6a216b84a546b88c3ee16e59c33dd0';// hide later
 
 //Set up get/post routes
 
-let projectData = {};
 
-app.get ("/Data", function getData(req,res) {
-    res.send (projectData);
+
+//app.get ("/", function getData(req,res) {
+ //   res.send ('dist/index.html');
+//})
+let tripData = {};
+
+app.post ("/Client", postClientData) 
+async function postClientData(req,res) {
+    let clientData = req.body;
+    let newClientData = {
+        destination: clientData.city,
+        start: clientData.departure,
+        end: clientData.returnDate
+    }
+    tripData = newClientData;
+    res.send(tripData);
 }
-)
 
-app.post ('/Data', addData);
- async function addData (req,res) {
-    const data = req.body;
-    let projectData = data;
-    console.log (projectData);
-    const geoApi = await fetch (`${baseGeoUrl}&q=${data.city}&username=${username}${geoP}`, {method:'POST'})
-    try {
-        const geoData = await geoApi.json();
-        projectData['long'] = geoData.geonames.lng;
-        projectData['lat'] = geoData.geonames.lat;
-        console.log('apiData ++++>', projectData)
-        res.send(projectData)
-    }
-    catch (error) {console.log('Error', error)}
- }
+app.get ('/Coordinates', getData);
+async function getData (req,res) {
 
-
- app.get ("/WeatherInfo", getWeather);
-
- async function getWeather (req,res) {
-    const long = projectData.long;
-    const lat = projectData.lat;
-    console.log(`Request latitude is ${projectData.lat}`);
-    console.log(`Request longitude is ${projectData.long}`);
-    const weatherCall = `${baseWeatherUrl}&lat=${lat}&lon=${long}&key=${apiK}`;
-    console.log(`Weatherbit URL is ${weatherCall}`);//hide later
-    try {
-        const responde = await fetch (weatherCall);
-        if (!response.ok) {
-            console.log(`Error. Response status ${response.status}`);
-            res.send(null);
+    console.log('GET geonamesData');
+    
+    const geoURL = `${baseGeoUrl}&q=${tripData.destination}&username=${username}${geoP}`; 
+    console.log("This is GEO url: " + geoURL);
+    fetch(geoURL)
+    .then (res => res.json())
+    .then (response => {
+        try{
+            console.log('Information from GeoNames')
+            console.log(response);
+            tripData['long'] = response.geonames[0].lng;
+            tripData['lat'] = response.geonames[0].lat;
+            tripData['name'] =response.geonames[0].name; 
+            tripData['adminName'] = response.geonames[0].adminName1;
+            tripData['countryName'] = response.geonames[0].countryName;
+            tripData['code'] = response.geonames[0].countryCode;
+            res.send(response);
         }
-        const weatherData = await responde.json();
-        projectData['icon'] = weatherData.data[0].weather.icon;
-        projectData['description'] = weatherData.data[0].weather.description;
-        projectData['temp'] = weatherData.data[0].temp;
-        res.send(weatherData);
-        console.log(weatherData);
-    }
-    catch (error) {
-        console.log(`Error connecting to server: ${error}`);
-        res.send(null);
- }}
+        catch (e) {
+            console.log("Error: ", e);
+          }
+        })
+}
+
+
+ app.get ("/Weather", getWeather);
+
+ function getWeather (req,res) {
+     const lng = tripData.long;
+     const lat = tripData.lat;
+    console.log ('Getting Weather')
+    const weatherURL = `${baseWeatherUrl}&lat=${lat}&lon=${lng}&key=${apiK}`;
+    console.log(`This is URL for weather API: ${weatherURL}`);
+    fetch (weatherURL)
+    .then (res => res.json())
+    .then (response => {
+       
+            const data = response.data;
+            tripData.temp = data.temp;
+            tripData.city = data.city_name;
+            tripData.clouds = data.clouds;
+            res.send(response);
+            console.log(response);
+        console.log ( tripData.city)
+    })}
+
+
+
+
+
