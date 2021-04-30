ce40dfa709d8e0e099c42d192e5a71e1f8503832
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-04-30 15:14:27 -0700

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,13 @@
-imo
+import { actionS, 
+    postData, 
+    callServer,  
+    updateUIcurrent,  
+   updateUiPredicted } from './js/app';
+
+   export { actionS, 
+    postData, 
+    callServer, 
+    updateUIcurrent, 
+    updateUiPredicted, 
+   };
+   
