46f4c4a0efc6ca5aa45a0562fde682d3f919d1cc
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-04-29 17:08:35 -0700

    Update getInfo.js

--- src/client/js/getInfo.js
+++ src/client/js/getInfo.js
@@ -1,28 +1,42 @@
 
- 
+ // Create an async onclick function
+
  async function actionS() {
     event.preventDefault();
     const city = document.getElementById('cityinput').value;
     const departureDate = document.getElementById('departureDate').value;
     const returnDate = document.getElementById('return').value;
 
+ 
+
+ // Checking input values
+
     if (city != '' && departureDate != ''  && returnDate != '') 
+    
     {await postData("http://localhost:3100/Client", {
       city: city,
       departure: departureDate,
       returnDate: returnDate,
     });
-   
+    
+
+   // Getting data from server
+
     await callServer(`/Coordinates`);
     await callServer(`/Weather`);
     await callServer(`/Pictures`);
     const trip = await callServer(`/Trip`);
     console.log(trip);
-    updateUI();
-    } 
+    updateUI(); }
     else {console.log ("Invalid iput, try again!")}
- }
+    //Set up dates duration
 
+    let startDate = new Date(departureDate);
+    let endDate = new Date(returnDate);
+    let tripDuration = ("Days in a trip: " + parseInt((endDate.getTime()-startDate.getTime())/(24*3600*1000)));
+    console.log(tripDuration)
+    return tripDuration;
+  } 
 
  async function postData(url, data = {}) {
     const response = await fetch(url, {
@@ -53,17 +67,25 @@
       }
 }
 
+//Add a function for displaying Trip data
+
 async function updateUI() {
   const response = await fetch ("/Trip");
   const uData = await response.json();
-  const pic = document.getElementById('pic');
   const weather = document.getElementById('weather');
+  const pic = document.getElementById('pic');
   const cityoutput = document.getElementById('cityoutput'); 
   const outputdates = document.getElementById('outputdates'); 
-  pic.innerHTML =  uData.webformatURL;
+  pic.src = uData.image;
   weather.innerHTML = `Current temperature is: ${uData.temp}°, ${uData.description.toLowerCase()}`;
   cityoutput.innerHTML = `${uData.destination}, ${uData.countryName}`;
-  outputdates.innerHTML = `Trip dates: ${uData.start} to ${uData.end}`;
+  outputdates.innerHTML = `Trip dates: ${uData.start} to ${uData.end}, ${tripDuration}`;
 
 }
+
+//Get duration of the trip
+
+//function tripDuration(){
+
+ 
 
