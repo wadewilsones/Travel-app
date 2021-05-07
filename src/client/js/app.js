aee9405019e333da03cfd0dfcdc23d478a94a514
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-05-07 14:31:19 -0700

    Add files via upload

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,139 +1,119 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser'); 
-const cors = require('cors');
-const fetch = require('node-fetch');
-const { response } = require('express');
 
+document.addEventListener("DOMContentLoaded", function ready(){
+document.getElementById('sub-btn').addEventListener ("click",  actionS);
+ async function actionS(e) {
+    e.preventDefault();
+    const city = document.getElementById('cityinput').value;
+    const departureDate = document.getElementById('departureDate').value;
+    const returnDate = document.getElementById('return').value;
 
-app.use (bodyParser.urlencoded ({extended:false}));
-app.use (bodyParser.json());
-app.use (cors());
-app.use (express.static('../../src/client'));
+ // Checking input values
 
-
-//Set up a server
-
-const port = 3100;
-const server = app.listen (port, listening);
-function listening () {
-    console.log ('Server is running on localhost:' + port)
-}
-
-
-//Set up API links
-
-const baseGeoUrl = 'http://api.geonames.org/searchJSON?';
-const username = 'wadewilso'; // hide later
-const geoP = "&maxRows=1";
-
-
-const apiK = '5a6a216b84a546b88c3ee16e59c33dd0';// hide later
-
-const basePixabayURL = "https://pixabay.com/api/";
-const apiPix = "20411781-12da6a01e91bbdaac4fb1f75f"; // hide later
-
-
-//Set up get/post routes
-
-//app.get ("/", function getData(req,res) {
- //   res.send ('dist/index.html');
-//})
-
-// Creation of a project's information holder
-let tripData = {};
-
-app.post ("/Client", postClientData) 
-async function postClientData(req,res) {
-    let clientData = req.body;
-    let newClientData = {
-        destination: clientData.city,
-        start: clientData.departure,
-        end: clientData.returnDate,
-        tripDuration: clientData.tripDuration
-    }
-    tripData = newClientData;
-    res.send(tripData);
-}
-
-app.get ('/Coordinates', getData);
-async function getData (req,res) {
-
-    console.log('GET geonamesData');
+    if (city != '' && departureDate != ''  && returnDate != '') 
     
-    const geoURL = `${baseGeoUrl}&q=${tripData.destination}&username=${username}${geoP}`; 
-    console.log("This is GEO url: " + geoURL);
-    fetch(geoURL)
-    .then (res => res.json())
-    .then (response => {
-        try{
-            console.log('Information from GeoNames')
-            console.log(response);
-            tripData['long'] = response.geonames[0].lng;
-            tripData['lat'] = response.geonames[0].lat;
-            tripData['name'] =response.geonames[0].name; 
-            tripData['adminName'] = response.geonames[0].adminName1;
-            tripData['countryName'] = response.geonames[0].countryName;
-            tripData['code'] = response.geonames[0].countryCode;
-            res.send(response);
-        }
-        catch (e) {
-            console.log("Error: ", e);
-          }
-        })
-}
+     // Set up the trip duration
+    { let startDate = new Date(departureDate);
+      let endDate = new Date(returnDate);
+      let tripDuration = (parseInt((endDate.getTime()-startDate.getTime())/(24*3600*1000)));
+      console.log(tripDuration)
+      
+      await postData("http://localhost:3100/Client", {
+      city: city,
+      departure: departureDate,
+      returnDate: returnDate,
+      tripDuration: tripDuration
+    });
+      await callServer(`/Coordinates`);
+      await callServer(`/Weather`);
+      await callServer(`/Pictures`);
+      const trip = await callServer(`/Trip`);
+      console.log(trip);
+      
+      //Predicted or current weather data
+      console.log ("Departure date is " + startDate);
+      let today = new Date();
+      console.log ("Today is " + today);
+      let weekdates = parseInt((startDate.getTime() - today.getTime())/(24*3600*1000));
+      console.log ("Days before leaving:" + weekdates)
+      if (weekdates < 7)
+      {updateUIcurrent()}
+      else{updateUiPredicted()}
 
-app.get ("/Weather", getWeather);
-const baseWeatherUrl = 'https://api.weatherbit.io/v2.0/forecast/daily?';
-async function getWeather (req,res) {
-     const lng = tripData.long;
-     const lat = tripData.lat;
-    console.log ('Getting Weather')
-    const weatherURL = `${baseWeatherUrl}&lat=${lat}&lon=${lng}&key=${apiK}`;
-    console.log(`This is URL for weather API: ${weatherURL}`);
-    try {
-        const response = await fetch(weatherURL);
-        
-        if (!response.ok) {
-            console.log(`Error connecting to Weatherbit API. Response status ${response.status}`);
-            res.send(null);
-        }
-        const weatherbitData = await response.json();
-        tripData['datetime'] = weatherbitData.data[0].datetime;
-        tripData['icon'] = weatherbitData.data[0].weather.icon;
-        tripData['description'] = weatherbitData.data[0].weather.description;
-        tripData['tempcurrent'] = weatherbitData.data[0].temp;
-        tripData['tempPredicted'] = weatherbitData.data[15].temp;
-        res.send(weatherbitData);
-        console.log(weatherbitData);
-        // If failed connection to API, return null
-    } catch (error) {
-        console.log(`Error connecting to server: ${error}`);
-        res.send(null);
-    }    
-        
+     }
+    else { alert ("Invalid iput, try again!")
+      console.log ("Invalid iput, try again!")}
     }
+  })
 
-app.get ("/Pictures", getPictures);
-async function getPictures(req,res){
-    console.log('GET pictures');
-    const picURL = `${basePixabayURL}?key=${apiPix}&q=${tripData.destination}&image_type=photo&orientation=horizontal`;
-    console.log(`This is URL for pic API: ${picURL}`);
-    fetch(picURL)
-    .then (res => res.json())
-    .then (response => {
-        const data = response.hits;
-        tripData.image = data[1].webformatURL;
-        console.log (tripData.image);
-        res.send (tripData);
+ async function postData(url, data = {}) {
+    const response = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+ }
 
-    })
+ const callServer = async(url) => {
+  const asyncParams = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {'Content-Type': 'application/json;charset=utf-8'}
+  };
+
+    const res = await fetch(url, asyncParams);
+      try{
+        const data = await res.json();
+        return data;
+      } 
+      catch {
+        console.log(`Error: ${res.statusText}`)
+      }
+}
+ 
+
+//Add a function for displaying Trip data
+
+async function updateUIcurrent() {
+  const response = await fetch ("/Trip");
+  const uData = await response.json();
+  const weather = document.getElementById('weather');
+  const pic = document.getElementById('pic');
+  const cityoutput = document.getElementById('cityoutput'); 
+  const outputdates = document.getElementById('outputdates'); 
+  pic.src = uData.image;
+  weather.innerHTML = `<span style= "font-size:18px">Current temperature:</span> <br/> <span style= "font-size:96px; line-height:1;"> ${uData.tempcurrent}°</span><br/><span style= "font-size:24px; line-height:0.2;margin-bottom:20px;"> ${uData.description.toLowerCase()}</span>`;
+  cityoutput.innerHTML = `<span style= "font-size:18px">Destination:</span><br/>${uData.destination}, ${uData.countryName}`;
+  outputdates.innerHTML =`<span style= "font-size:18px">Trip Duration:</span><br/> <span style= "font-size:72px;line-height:1"> ${uData.tripDuration} days</span><br/><span style= "font-size:24px"> ${uData.start} - ${uData.end}</span>`;
 
 }
 
-app.get ("/Trip", getFullData)
+async function updateUiPredicted() {
+  const response = await fetch ("/Trip");
+  const uData = await response.json();
+  const weather = document.getElementById('weather');
+  const pic = document.getElementById('pic');
+  const cityoutput = document.getElementById('cityoutput'); 
+  const outputdates = document.getElementById('outputdates'); 
+  pic.src = uData.image;
+  weather.innerHTML = `<span style= "font-size:18px">Predicted temperature: </span> <br/> <span style= "font-size:96px;line-height:1">${uData.tempPredicted}°</span><br/><span style= "font-size:24px; line-height:0.2; margin-bottom:20px;"> ${uData.description.toLowerCase()}</span>`;
+  cityoutput.innerHTML = `<span style= "font-size:18px">Destination:</span><br/>${uData.destination}, ${uData.countryName}`;
+  outputdates.innerHTML = `<span style= "font-size:18px">Trip Duration:</span><br/><span style= "font-size:72px;line-height:1"> ${uData.tripDuration}  days</span><br/><span style= "font-size:24px">${uData.start} - ${uData.end}</span>`;
+
+}
+
  
-function getFullData(req,res) {
-    console.log (tripData);
-    res.send (tripData);
+
+
+//Export functions
+
+export { 
+ 
+  callServer,
+  updateUIcurrent,
+  updateUiPredicted
+
 }
